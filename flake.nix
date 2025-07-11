f62dfa570fbb729e712b5c1a6e2ab40d7c90736b
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2025-07-11 13:41:38 +0200

    Fix flake for 1.2.1

--- flake.nix
+++ flake.nix
@@ -27,11 +27,11 @@
           default = self.packages.${system}.pangolin-newt;
           pangolin-newt = pkgs.buildGoModule {
             pname = "pangolin-newt";
-            version = "1.1.3";
+            version = "1.2.1";
 
             src = ./.;
 
-            vendorHash = "sha256-sTtiBBkZ9cuhWnrn2VG20kv4nzNFfdzP5p+ewESCjyM=";
+            vendorHash = "sha256-Yc5IXnShciek/bKkVezkAcaq47zGiZP8vUHFb9p09LI=";
 
             meta = with pkgs.lib; {
               description = "A tunneling client for Pangolin";
